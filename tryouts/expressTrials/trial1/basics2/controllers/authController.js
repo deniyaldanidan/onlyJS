f7ab1781131bee2c0cd5e2fe3b44627d8a374306
deniyaldanidan <deniyaldanidan@gmail.com>
2022-11-04 07:29:52 +0000

    updates

--- tryouts/expressTrials/trial1/basics2/controllers/authController.js
+++ tryouts/expressTrials/trial1/basics2/controllers/authController.js
@@ -21,12 +21,12 @@ const handleLogin = async (req, res)=>{
                 }
             },
             process.env.ACCESS_TOKEN_SECRET,
-            {expiresIn: '30s'}
+            {expiresIn: '20s'}
         )
         const refreshToken = jwt.sign(
             {username: foundUser.username},
             process.env.REFRESH_TOKEN_SECRET,
-            {expiresIn: '1d'}
+            {expiresIn: '1h'}
         )
         // saving refreshToken in DB
         foundUser.refreshToken = refreshToken;
