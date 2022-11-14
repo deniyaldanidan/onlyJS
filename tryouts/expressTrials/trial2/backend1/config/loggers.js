7ffd31467f05be91533fb7dd21d24d17e1d82139
deniyaldanidan <deniyaldanidan@gmail.com>
2022-11-14 05:08:55 +0000

    updates

--- tryouts/expressTrials/trial2/backend1/config/loggers.js
+++ tryouts/expressTrials/trial2/backend1/config/loggers.js
@@ -1,8 +1,17 @@
 const fs = require('fs');
 const path = require('path')
 
+if (!fs.existsSync(path.join(__dirname, '..', 'logs'))){
+    fs.mkdir(path.join(__dirname, '..', 'logs'), err=>{
+        if(err){
+            throw err;
+        }
+    });
+}
+
 const infoStream = fs.createWriteStream(path.join(__dirname, '..', 'logs', 'info.log'), { flags: "a" });
 const errorStream = fs.createWriteStream(path.join(__dirname, '..', 'logs', 'error.log'), { flags: "a" });
+
 
 const infoLog = (message, log = true) => {
     const data = `${new Date().toLocaleString('en-GB', {timeZone: "IST"})} ${message}\n`;
