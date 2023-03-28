9eba01726a275654bfe1b9089806b1c17e76aab8
deniyaldanidan <deniyaldanidan@gmail.com>
2023-03-28 09:28:37 +0000

    authed-blog-app is done

--- nextjs/crash-apps/authed-blog-app/README.md
+++ nextjs/crash-apps/authed-blog-app/README.md
@@ -1,5 +1,8 @@
 # Authed Blog App
 
+## Description
+Building this app to learn nextjs & next-auth & nodemailer-basics
+
 ## TODOS
 
 - [x] Implement Dark/Light Theme using ContextApi-LocalStorage
@@ -11,44 +14,12 @@
 
 - [x] Experiment with nodemailer
 
-- [ ] Implement a Auth-Strategy using next-auth [Email, Google, Github, Credentials].
+- [x] Implement a Auth-Strategy using next-auth [Email, Google, Github, Credentials].
     - [x] Create a custom adapter for mongoose
     - [x] Add OAuth providers
     - [x] Add Email-Strategy use nodemailer
-    - [ ] Custom auth pages *
+    - [x] Custom auth pages
 
-- [ ] NextJS Middleware
 - [x] Implement protected pages
 - [x] Implement protected APIs (using custom-callbacks-method)
 
-
-## Reminders
-- Create a seperate folder to populate the mongoose database
-
-## Pages
-- [ ] home
-- [ ] about
-- [ ] blogs with search, sort and filter
-- [ ] blogs/:slug with only likes & dislikes
-- [ ] login
-- [ ] register
-- [ ] profile
-
-- [ ] admin-page
-
-- [ ] editor-page
-
-## Home Page Structure
-- [x] Hero
-- [x] Subscribe
-- [ ] 10 Recent-Blogs
-- [ ] 3 from random Cat-1
-- [ ] 3 from random Cat-2
-
-## Used technologies
-- [ ] Mongoose [for database]
-- [ ] framer-motion [for animations & page-transitions]
-- [ ] useSWR [for client-side-data fetching]
-- [x] sass
-- [ ] any text-editor
-- [ ] clsx for conditional classnames
--- nextjs/crash-apps/authed-blog-app/src/pages/auth/signIn.tsx
+++ nextjs/crash-apps/authed-blog-app/src/pages/auth/signIn.tsx
@@ -1,28 +1,45 @@
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next"
 import { getServerSession } from "next-auth/next";
-import { getProviders } from "next-auth/react";
+import { getCsrfToken, getProviders, signIn } from "next-auth/react";
 import { authOptions } from "../api/auth/[...nextauth]";
-import {FcGoogle} from 'react-icons/fc';
-import {FaGithub} from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
 import styles from '@/styles/signIn.module.scss';
 
 
-export default function SignIn({providers}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function SignIn({ providers, csrfToken }: InferGetServerSidePropsType<typeof getServerSideProps>) {
 
-    console.log(providers);
+    // console.log(providers);
 
     return (
-        <div>
-            <div className={styles.oauth}><span>Sign Up using:</span> <FcGoogle /> <span>OR</span> <FaGithub /> </div>
+        <div className={styles.signInContainer}>
+            <div className={styles.contHead}>Sign Up Using</div>
+            <div className={styles.oauth}>
+                <FcGoogle className={styles.oauthSym} onClick={()=>signIn('google')} />
+                <span>Or</span>
+                <FaGithub className={styles.oauthSym} onClick={()=>signIn('github')} />
+            </div>
+            <div className={styles.orSym}>Or</div>
+            <div>
+                <form method="post" action="/api/auth/signin/email">
+                    <input type="hidden" name="csrfToken" defaultValue={csrfToken} />
+                    <div className={styles.inpGrp}>
+                        <label htmlFor="email"> Using Email</label>
+                        <input type="email" name="email" id="email" placeholder="example@example.com" />
+                    </div>
+                    <button>Log In</button>
+                </form>
+            </div>
         </div>
     )
 }
 
 
-export async function getServerSideProps(context:GetServerSidePropsContext){
+export async function getServerSideProps(context: GetServerSidePropsContext) {
     const session = await getServerSession(context.req, context.res, authOptions);
+    const csrfToken = await getCsrfToken(context);
 
-    if (session){
+    if (session || !csrfToken) {
         return { redirect: { destination: context.query?.callbackUrl || "/" } };
     }
 
@@ -30,7 +47,8 @@ export async function getServerSideProps(context:GetServerSidePropsContext){
 
     return {
         props: {
-            providers: providers ?? []
+            providers: providers ?? [],
+            csrfToken
         }
     }
 }
